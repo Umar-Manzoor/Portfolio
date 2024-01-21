f49497539b65856f2cc074a66cfe6ad419973f2c
Umar Manzoor <umanzoor3@gatech.edu>
2024-01-21 18:53:00 -0500

    filled in descriptions for everything -complete

--- src/sections/About.js
+++ src/sections/About.js
@@ -28,14 +28,14 @@ const About = () => {
                             </div>
                         </div>
                         <div className="text-container">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>I'm a Computer Science student set to graduate in May 2024 with a Bachelors degree in Computer Science from Georgia Tech. Beyond my academic pursuits, I'm deeply involved in extracurricular activities that reflect my commitment to personal growth and community involvement. I've held leadership roles in various student organizations at Georgia Tech. Additionally, I've contributed heavily to the Muslim population in Georgia by helping plan the largest Islamic conference in the southeast. In the realm of software development, I'm skilled at building fullstack projects with modern enterprise level technologies. Serving as a Resident Assistant in a freshman dorm, I've fostered a sense of community among new students. As I near graduation, I'm excited to leverage my experiences for a fulfilling career in software engineering.</p>
                         </div>
                     </div>
                     <div className="section__pic-container">
                         <img 
                             src={require("../assets/about-pic.png")} 
                             alt="Umar Manzoor about pic"
-                            className="about-pic"
+                            className="about-pic-intro"
                         />
                     </div>
                 </div>
@@ -55,7 +55,7 @@ const About = () => {
                             <h1 className="about-title">Vice President</h1>
                         </div>
                         <div className="text-container">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>I held a pivotal role in steering the organization's activities and nurturing a thriving community. Leading a dedicated committee of 10 members, I orchestrated a diverse range of events, including weekly religious lectures, monthly gatherings with 50+ attendees, and large-scale semesterly events with 200+ participants. Beyond event planning, I played a crucial role in shaping the organization's vision and strategy, collaborating closely with the President and other board members to ensure the association's goals were met. Additionally, I organized and facilitated dinners during the sacred month of Ramadan, offering essential support to Muslim students. </p>
                         </div>
                         <div>
                             <img 
@@ -76,7 +76,7 @@ const About = () => {
                             <h1 className="about-title">Conference Organizer</h1>
                         </div>
                         <div className="text-container-miftaah">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>As a member of the conference organizing team at Miftaah Institute, I played a crucial role in planning the largest Islamic conference in the Southeastern United States. My responsibilities included event marketing, logistical coordination, and leading a team of 60 volunteers on the conference day. We successfully organized an event that brought together 1,800+ attendees and featured seven world-renowned religious scholars as speakers. I continue to serve in this role for the upcoming 2024 conference, where I'm committed to ensuring its success.</p>
                         </div>
                         <div>
                         <img 
@@ -111,7 +111,7 @@ const About = () => {
                             <h1 className="about-title">Treasurer</h1>
                         </div>
                         <div className="text-container">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>I took on the responsibility of managing the organization's finances, overseeing a substantial annual budget of $20,000. My duties included budgeting, applying for grants, and fundraising to support the association's initiatives. Notably, I successfully secured funding from the Consulate General of Pakistan in Dallas, Texas, further strengthening the financial foundation of the organization. In addition to financial management, I fulfilled all the essential duties expected of a treasurer within a student organization, contributing to the overall success of the Pakistani Student Association.</p>
                         </div>
                     </div>
                 </div>
@@ -124,7 +124,7 @@ const About = () => {
                             <h1 className="about-title">Resident Assistant</h1>
                         </div>
                         <div className="text-container-miftaah">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>As a Resident Assistant (RA) in a freshman dorm at Georgia Tech, I hold a multifaceted position that plays a crucial role in shaping the residential experience for our new students. My responsibilities encompass diverse areas, including offering academic guidance, organizing community-building events, ensuring safety and security, providing mentorship, managing administrative tasks, assisting during emergencies, designing engaging programs, and mediating conflicts. My dedication to creating a welcoming and supportive environment for incoming freshmen contributes significantly to their personal growth and academic success. I take pride in fostering a sense of belonging and community within the dorm, enabling our residents to thrive during their crucial first year at Georgia Tech.</p>
                         </div>
                     </div>
                     <div className="section__pic-container-msa">
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Techedia</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>Created an interactive social media web application exclusively for Georgia Tech students, featuring user authentication and seamless file upload/download capabilities. The project leveraged a modern tech stack, including ReactJS for a dynamic frontend, Node.js for a scalable backend, Express.js for middleware management, and MongoDB for efficient database handling. Security was paramount, and I ensured database protection through mandatory user authentication using JSON Web Tokens. Rigorous unit testing with Postman resulted in an impressive 95% code coverage, allowing for quick identification and resolution of any issues.</p>
                         </div>
                         <div className="btn-container">
                         <button
@@ -22,7 +22,7 @@ const Projects = () => {
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Swiftly</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>Led a development team in the creation of a cutting-edge mobile application that redefined the in-store shopping experience. The app empowered users to scan products, add them to their cart, and complete purchases seamlessly on their phones, eliminating the need for long checkout lines. The tech stack included React-Native for the frontend, AWS DynamoDB for robust database management, Node.js for the backend, and Stripe for secure transactions. User testing demonstrated Swiftly's remarkable performance, with it being up to 29 times faster during peak shopping times and around 10 times faster during normal hours. Notably, our project secured 2nd place in Georgia Tech's Create-X class, competing against 16 startups.</p>
                         </div>
                         <div className="btn-container">
                         <button
@@ -32,11 +32,19 @@ const Projects = () => {
                             Github
                         </button>
                         </div>
+                        <div className="btn-container-swiftly">
+                        <button
+                            className="btn btn-color-2 project-btn"
+                            onClick={() => window.location.assign('https://www.linkedin.com/feed/update/urn:li:activity:7140239145741852672/')} 
+                        >
+                            LinkedIn
+                        </button>
+                        </div>
                     </div>
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Student Org Event Organizer</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>Created an interactive website aimed at simplifying event planning and organization for Georgia Tech students involved in student organizations. The platform offered features like RSVPs and geolocation-based event mapping, enhancing the overall experience. The tech stack included ReactJS for the frontend, MongoDB for efficient database management, Next.js as the backend framework, and Node.js for backend development. Notably, the website contributed to a significant 25% increase in student engagement for nine student organization events, each with attendee counts ranging from 50 to 200 people during the spring semester.</p>
                         </div>
                         <div className="btn-container">
                         <button
@@ -50,7 +58,7 @@ const Projects = () => {
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Georgia Tech Blog</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>Designed and developed a specialized blog platform catering to Georgia Tech students, offering a space for them to share and explore content relevant to their university experience. Leveraging React for the frontend and a local JSON server for backend data storage, I employed the useContext API for efficient state management. The platform allows users to create and read posts, fostering a sense of community among Georgia Tech students.</p>
                         </div>
                         <div className="btn-container">
                         <button
@@ -64,7 +72,7 @@ const Projects = () => {
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Portfolio</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>Developed a dynamic and engaging portfolio website using React for a component-based frontend design. Achieved a significant 40% boost in site rendering speed through meticulous JavaScript/HTML optimizations and the implementation of effective state management techniques. This project showcases my expertise in crafting high-performance web experiences with React.</p>
                         </div>
                         <div className="btn-container">
                         <button
@@ -78,7 +86,7 @@ const Projects = () => {
                     <div className="details-container color-container">
                         <h2 className="experience-sub-title project-title">Snake Game</h2>
                         <div className="text-container-projects">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere placeat, alias laborum culpa maxime autem recusandae cupiditate, esse saepe quis, quo libero porro non eius soluta necessitatibus tempora. Incidunt consequuntur eligendi commodi atque assumenda enim cumque, distinctio, dolore fugiat officia ut aperiam iste eaque et, deleniti minus quibusdam. Commodi.</p>
+                            <p>I developed a classic Snake game in Java, harnessing the power of JavaFX and Java Swing for graphics. JavaFX's flexibility and rich set of UI controls allowed me to create a visually engaging and responsive game interface, while Java Swing's ease of use and event-driven model enhanced the user experience. The game offers an immersive journey with smooth animations, intuitive controls, and a scoring system. It showcases my Java programming skills and game development expertise, illustrating how these technologies seamlessly combine traditional gameplay with the capabilities of JavaFX and Java Swing.</p>
                         </div>
                         <div className="btn-container">
                         <button
