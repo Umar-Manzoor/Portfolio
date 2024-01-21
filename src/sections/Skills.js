5ae1bb65e7a2c03f3530f610780dad3717dcec1e
Umar Manzoor <umanzoor3@gatech.edu>
2024-01-21 14:38:57 -0500

    updated skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -4,76 +4,111 @@ const Skills = () => {
             <p className="section__text__p1">Here Are My</p>
             <h1 className="title">Skills</h1>
             <div className="experience-details-container">
-                <div className="about-containers">
-                <div className="details-container">
-                    <h2 className="experience-sub-title">Frontend Development</h2>
-                    <div className="article-container">
+                <div className="skills-containers">
+                    <div className="details-container">
+                        <h2 className="experience-sub-title">Programming Languages</h2>
+                        <div className="article-container">
+                            <article>
+                                <div>
+                                    <h3>Python</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>Java</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>JavaScript</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>GoLang</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>C</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>C++</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>HTML</h3>
+                                </div>
+                            </article>
+                            <article>
+                                <div>
+                                    <h3>CSS</h3>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <div className="details-container">
+                        <h2 className="experience-sub-title">Technologies</h2>
+                        <div className="article-container">
                         <article>
                             <div>
-                                <h3>HTML</h3>
+                                <h3>ReactJS</h3>
                             </div>
                         </article>
                         <article>
                             <div>
-                                <h3>CSS</h3>
+                                <h3>React-Native</h3>
                             </div>
                         </article>
                         <article>
                             <div>
-                                <h3>JavaScript</h3>
+                                <h3>Node js</h3>
                             </div>
                         </article>
                         <article>
                             <div>
-                                <h3>React JS</h3>
+                                <h3>Express js</h3>
                             </div>
                         </article>
                         <article>
                             <div>
-                                <h3>ReactNative</h3>
+                                <h3>Git</h3>
+                            </div>
+                        </article>
+                        <article>
+                            <div>
+                                <h3>Postman</h3>
+                            </div>
+                        </article>
+                        <article>
+                            <div>
+                                <h3>AWS S3</h3>
+                            </div>
+                        </article>
+                        <article>
+                            <div>
+                                <h3>MongoDB</h3>
+                            </div>
+                        </article>
+                        <article>
+                            <div>
+                                <h3>NATS.io</h3>
                             </div>
                         </article>
                         </div>
                     </div>
-                        <div className="details-container">
-                            <h2 className="experience-sub-title">Backend Development</h2>
-                            <div className="article-container">
-                            <article>
-                                <div>
-                                    <h3>MongoDb</h3>
-                                </div>
-                            </article>
-                            <article>
-                                <div>
-                                    <h3>Node JS</h3>
-                                </div>
-                            </article>
-                            <article>
-                                <div>
-                                    <h3>Express JS</h3>
-                                </div>
-                            </article>
-                            <article>
-                                <div>
-                                    <h3>Git</h3>
-                                </div>
-                            </article>
-                            <article>
-                                <div>
-                                    <h3>NATS</h3>
-                                </div>
-                            </article>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <img 
-                        src={require("../assets/arrow.png")}
-                        alt="Arrow icon" 
-                        className="icon arrow" 
-                        onClick={() => window.location.assign('./#projects')} 
-                    />
-                    </section>
+                </div>
+            </div>
+            <img 
+                src={require("../assets/arrow.png")}
+                alt="Arrow icon" 
+                className="icon arrow" 
+                onClick={() => window.location.assign('./#projects')} 
+            />
+        </section>
     )
 }
 
